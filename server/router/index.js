166a0b8d653efa590729eaa8f5aef009e41bb0a5
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-05-02 06:21:42 +0300

    Finish backend relates #3

---
+++ server/router/index.js
@@ -0,0 +1,14 @@
+const addTask = require('../controllers/addTask');
+const deleteTask = require('../controllers/deleteTask');
+const editTask = require('../controllers/editTask');
+const getTasks = require('../controllers/getTasks');
+
+const router = require('express').Router();
+
+
+router.post('/task', addTask);
+router.get('/task', getTasks);
+router.delete('/task/remove/:taskId', deleteTask)
+router.patch('/task/edit/:taskId', editTask)
+
+module.exports = router;
