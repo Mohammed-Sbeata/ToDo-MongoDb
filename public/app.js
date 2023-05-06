f443a6c9cd60e5478f19876ba8e7417c7e6ee01d
Eng-Mohammed-Sbeata <majd86787@gmail.com>
2023-05-06 17:39:04 +0300

    finish Project relates #7

--- public/app.js
+++ public/app.js
@@ -1,67 +1,3 @@
-// window.addEventListener('load', () => {
-// 	const form = document.querySelector("#new-task-form");
-// 	const input = document.querySelector("#new-task-input");
-// 	const list_el = document.querySelector("#tasks");
-
-
-// 	form.addEventListener('submit', (e) => {
-// 		e.preventDefault();
-
-// 		const task = input.value;
-
-// 		const task_el = document.createElement('div');
-// 		task_el.classList.add('task');
-
-// 		const task_content_el = document.createElement('div');
-// 		task_content_el.classList.add('content');
-
-// 		task_el.appendChild(task_content_el);
-
-// 		const task_input_el = document.createElement('input');
-// 		task_input_el.classList.add('text');
-// 		task_input_el.type = 'text';
-// 		task_input_el.value = task;
-// 		task_input_el.setAttribute('readonly', 'readonly');
-
-// 		task_content_el.appendChild(task_input_el);
-
-// 		const task_actions_el = document.createElement('div');
-// 		task_actions_el.classList.add('actions');
-		
-// 		const task_edit_el = document.createElement('button');
-// 		task_edit_el.classList.add('edit');
-// 		task_edit_el.innerText = 'Edit';
-
-// 		const task_delete_el = document.createElement('button');
-// 		task_delete_el.classList.add('delete');
-// 		task_delete_el.innerText = 'Delete';
-
-// 		task_actions_el.appendChild(task_edit_el);
-// 		task_actions_el.appendChild(task_delete_el);
-
-// 		task_el.appendChild(task_actions_el);
-
-// 		list_el.appendChild(task_el);
-
-// 		input.value = '';
-
-// 		task_edit_el.addEventListener('click', (e) => {
-// 			if (task_edit_el.innerText.toLowerCase() == "edit") {
-// 				task_edit_el.innerText = "Save";
-// 				task_input_el.removeAttribute("readonly");
-// 				task_input_el.focus();
-// 			} else {
-// 				task_edit_el.innerText = "Edit";
-// 				task_input_el.setAttribute("readonly", "readonly");
-// 			}
-// 		});
-
-// 		task_delete_el.addEventListener('click', (e) => {
-// 			list_el.removeChild(task_el);
-// 		});
-// 	});
-// });
-
 const list_el = document.querySelector("#tasks");
 const input = document.getElementById("new-task-input");
 const addBtn = document.getElementById('new-task-submit');
@@ -125,39 +61,36 @@ const showData = (result)=>{
 		input.value = '';
 
 		task_edit_el.addEventListener('click', (e) => {
+			task_edit_el.style.display='none'
+
             const saveBtn = document.createElement('button')
 		    saveBtn.className = 'save'
 		    saveBtn.innerText = 'Save';
-			
-			fetch(`/task/edit/${item._id}`, {
-				method: 'PATCH',
-				headers: { 'Content-Type': 'application/json'},
-				body: JSON.stringify({
-				  task: input.value
-				})
-			  })
-			  .then((data) => data.json())
-			  .then((data) => {
-				if(!data.error){
-				  if (task_edit_el.innerText.toLowerCase() == "edit") {
-					  task_edit_el.innerText = "Save";
-					  task_input_el.removeAttribute("readonly");
-					  task_input_el.focus();
-				  } else {
-					  task_edit_el.innerText = "Edit";
-					  task_input_el.setAttribute("readonly", "readonly");
-				  }
-				}else{
-					throw new Error(data.message);
-				}
+			task_actions_el.appendChild(saveBtn)
+			task_input_el.removeAttribute('readonly')
+			task_input_el.focus()
+			saveBtn.addEventListener('click',()=>{
 
-			  })
-			  .catch((error) => window.alert(error.message))
-
-			
+				fetch(`/task/edit/${item._id}`, {
+					method: 'PATCH',
+					headers: { 'Content-Type': 'application/json'},
+					body: JSON.stringify({
+					  task: task_input_el.value
+					})
+				  })
+				  .then((data) => data.json())
+				  .then((data) => {
+					if(!data.error){
+						task_edit_el.style.display='block'
+					    saveBtn.style.display='none'
+					    task_content_el.textContent = task_input_el.value;
+					}else{
+						throw new Error(data.message);
+					}
+				  })
+				  .catch((error) => window.alert(error.message))
+			})
 		});
-
-
 		task_delete_el.addEventListener('click', (e) => {
 			list_el.removeChild(task_el);
 			fetch(`/task/remove/${item._id}`,{
--- server/database/query/editTaskQ.js
+++ server/database/query/editTaskQ.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const connection = require('../config/dbConnection');
 const taskM = require('../model/task')
 
-const editTaskQ = ({taskId, newTask}) => {
+const editTaskQ = ({taskId, task}) => {
     connection();
-    return taskM.updateOne({_id: new mongoose.Types.ObjectId(taskId)}, {$set: newTask})
+    return taskM.updateOne({_id: new mongoose.Types.ObjectId(taskId)}, {$set: {task}})
 }
 module.exports = editTaskQ;
